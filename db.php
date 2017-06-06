8be997e385cf800dbdf06ee6ea48c043e8ab22dc
samx81 <sam89881@gmail.com>
2017-06-06 13:18:39 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,30 @@
-
 <?php
-$serverName = "mssql5.gear.host"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"shorturls", "UID"=>"shorturls", "PWD"=>"Gf7UX!qRqzb!");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$db_host = 'mssql5.gear.host';
+$db_user = 'shorturls';
+$db_pass = 'Gf7UX!qRqzb!';
+$db_name = 'shorturls';
+$connectionInfo = array( "Database"=>$db_name, "UID"=>$db_user, "PWD"=>$db_pass);
+$conn = sqlsrv_connect( $db_host, $connectionInfo);
+
 if( $conn ) {
      echo "Connection established.<br />";
 }else{
      echo "Connection could not be established.<br />";
      die( print_r( sqlsrv_errors(), true));
 }
+
+$sql = "SELECT key,data FROM data WHERE key='test'";
+$stmt = sqlsrv_query( $conn, $sql);
+
+echo $stmt;
+
+function generateRandomString($length = 6) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
